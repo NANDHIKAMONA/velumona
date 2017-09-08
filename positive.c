fd60a151291ba0bd7f4d6fa288cb407f90fa5d2f
NANDHIKAMONA <31503954+NANDHIKAMONA@users.noreply.github.com>
2017-09-08 19:26:29 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int mail()
+{
+int n;
+printf("enter the number ");
+scanf("%d,&n");
+if(n>0)
+{
+print("the number is positive ");
+}
+if(n<0)
+{
+print ("the number is negative
+}
+if(n==0)
+{
+print("the number is zero");
+}
+}
